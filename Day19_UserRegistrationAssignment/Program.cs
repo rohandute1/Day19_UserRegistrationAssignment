ee3829cc1f6b85d55988c7b9b58aa476aa6c9a26
Rohan <rohandute1@gmail.com>
2023-06-27 00:37:25 +0530

    [Rohan] Successfully added Validation for first name

--- Day19_UserRegistrationAssignment/Program.cs
+++ Day19_UserRegistrationAssignment/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Day19_UserRegistrationAssignment
@@ -11,7 +12,28 @@ namespace Day19_UserRegistrationAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration assignment");
+            Console.WriteLine("Enter your first name:");
+            string firstName = Console.ReadLine();
+
+            // Validate the first name using regular expression
+            if (IsValidFirstName(firstName))
+            {
+                Console.WriteLine("Valid first name entered.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name entered.");
+            }
             Console.ReadLine();
+        }
+
+        static bool IsValidFirstName(string firstName)
+        {
+            // Regular expression pattern to match the first name condition
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+
+            // Check if the first name matches the pattern
+            return Regex.IsMatch(firstName, pattern);
         }
     }
 }
