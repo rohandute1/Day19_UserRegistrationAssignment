922ded23a784b79981d2b465b5603663bf8715f6
Rohan <rohandute1@gmail.com>
2023-06-27 00:45:30 +0530

    [Rohan] Successfully added restriction for user to write valid last name

--- Day19_UserRegistrationAssignment/Program.cs
+++ Day19_UserRegistrationAssignment/Program.cs
@@ -11,18 +11,20 @@ namespace Day19_UserRegistrationAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration assignment");
             Console.WriteLine("Enter your first name:");
             string firstName = Console.ReadLine();
 
-            // Validate the first name using regular expression
-            if (IsValidFirstName(firstName))
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+
+            // Validate the first name and last name using regular expressions
+            if (IsValidFirstName(firstName) && IsValidLastName(lastName))
             {
-                Console.WriteLine("Valid first name entered.");
+                Console.WriteLine("Valid first name and last name entered.");
             }
             else
             {
-                Console.WriteLine("Invalid first name entered.");
+                Console.WriteLine("Invalid first name or last name entered.");
             }
             Console.ReadLine();
         }
@@ -34,6 +36,15 @@ namespace Day19_UserRegistrationAssignment
 
             // Check if the first name matches the pattern
             return Regex.IsMatch(firstName, pattern);
+        }
+
+        static bool IsValidLastName(string lastName)
+        {
+            // Regular expression pattern to match the last name condition
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+
+            // Check if the last name matches the pattern
+            return Regex.IsMatch(lastName, pattern);
         }
     }
 }
